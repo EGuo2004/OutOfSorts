bcf795ea207ca6bf711238e14d2cfd3e8a1c1ddd
unknown <>
2020-12-21 00:11:46 -0500

    Adds InsertionSort

--- Sorts.java
+++ Sorts.java
@@ -4,15 +4,15 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    int temp;
+    int x;
     int swaps = 1;
     for (int i = data.length; i > 0 && swaps > 0;i--) {
         swaps = 0;
       for (int j = 1; j < i; j++) {
         if (data[j] < data[j-1]) {
-          temp = data[j];
+          x = data[j];
           data[j] = data[j-1] ;
-          data[j-1] = temp;
+          data[j-1] = x;
           swaps++;
         }
       }
@@ -20,22 +20,34 @@ public class Sorts{
   }
 
   public static void selectionSort(int[] data) {
-    int temp;
+    int x;
     int min_val;
     int indexOf;
     for (int i = 0; i < data.length - 1; i++) {
       min_val = data[i];
+      indexOf = i;
       for(int j = i + 1; j < data.length; j++) {
         if(data[j] < min_val) {
           min_val = data[j];
           indexOf = j;
         }
       }
-      temp = data[i];
-      data[i] = data[min_val];
-      data[j] = temp;
+      x = data[i];
+      data[i] = data[indexOf];
+      data[indexOf] = x;
     }
   }
 
+  public static void insertionSort (int[] data) {
+    int x;
+    for (int i = 1; i < data.length; i++) {
+      x = data[i];
+      int j;
+      for(j = i - 1 ; j >= 0 && x < data[j]; j--) {
+          data[j+1] = data[j];
+      }
+      data[j+1] = x;
+    }
+  }
 
 }
